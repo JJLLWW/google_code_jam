346c87df78409254c2e64ce14644d364d0c982af
Jack Wright <jack.louie.wright@gmail.com>
2022-06-04 23:02:23 +0100

    nothing works, take a break

---
+++ sidetrack/sidetrack1.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using ll = long long;
+
+// 1 - 1, 2 - 3, 3 - 4
+
+// WRONG
+int main() {
+    while(true) {
+        std::string line;
+        std::getline(std::cin, line);
+        if(line == "q") break;
+        int N = std::stoi(line);
+        ll npair = 0;
+        std::vector<int> squares(N);
+        for(int i=0; i<N; i++) {
+            squares[i] = i*i;
+        }
+        for(int i=1; i<=N; i++) {
+            for(int j=1; j<=N; j++) {
+                if(std::find(squares.begin(), squares.end(), i*j) != squares.end()) {
+                    npair++;
+                }
+            }
+        }
+        std::cout << npair << std::endl;
+    }
+}
